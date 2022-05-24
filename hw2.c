bf9c72c8a94cd3d742f61cb0cec7c2e32234d1c9
xitxxth <jongyn1128@gmail.com>
2022-05-25 00:49:59 +0900

    test

--- hw2.c
+++ hw2.c
@@ -6,5 +6,24 @@ int main(void)
         printf("FILE OPEN FAILED!\n");
         exit(0);
     }//file opened
-    
+    while(1){//begin while
+        int check;
+        check=fscanf(fp, "%s", command);//getcommand
+        if(check==EOF)    break;
+        if(!strcmp(command, "INSERT")){//Insert
+            fscanf(fp, "%d", &item);
+            insert(item);
+        }//end insert
+        /*else if(!strcmp(command, "ASCEND")){//Ascend
+            //fprintf
+        }//end ascend
+        else if(!strcmp(command, "DESCEND")){
+            //fprintf
+        }//end descend*/
+    }//end while
+    while(heap_size>0){
+        printf("%d", max_heap[heap_size]);
+    }
+    fclose(fp);
+    return 0;
 }
--- hw2.h
+++ hw2.h
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX_HEAP_SIZE 1000000
-
-int heap[MAX_HEAP_SIZE];//global array-heap
+#include <string.h>
+#define MAX_HEAP_SIZE 1000001
+#define MAX_COMMAND_SIZE 8
+int max_heap[MAX_HEAP_SIZE];//global array-heap
+int min_heap[MAX_HEAP_SIZE];
+char command[MAX_COMMAND_SIZE];
 void insert(int item);//insert item to the heap
 int delete(void);//delete item from the heap
+int item;
+int heap_size=0;
+
+void insert(int item)
+{//begin insert
+    if(heap_size==MAX_HEAP_SIZE-1){
+        printf("FULL\n");
+        return;
+    }//end if
+    max_heap[++heap_size] = item;//initially input
+    int i = heap_size;//for index
+    while(i>1){//begin while
+        if(item > max_heap[i/2]){//child > parent
+            max_heap[i] = max_heap[i/2];
+            i /= 2;
+        }
+    }//end while
+    max_heap[i] = item;//insert done
+
+
+}//end insert
+
+/*int delete(void)
+{
+
+}*/
